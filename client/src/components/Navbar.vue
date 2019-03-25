a27bb39f1c59a3406176c8b7ed12d98f5cf8b570
gadikpepe <gadikpepe76@gmail.comm>
2019-03-25 08:46:21 +0700

    Requirement Done

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <nav class="navbar fixed-top navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Hore Kanban</a>
+
+      <a href="#" data-toggle="modal" data-target="#new-task">
+        <i class="fas fa-tasks"></i>
+      </a>
+    </nav>
+    <InputNewTask/>
+  </div>
+</template>
+
+<script>
+import InputNewTask from '@/components/InputNewTask.vue';
+
+export default {
+  name: 'Navbar',
+  components: {
+    InputNewTask,
+  },
+};
+</script>
+
+<style>
+a {
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: none;
+}
+.navbar .fa-tasks {
+  font-size: 25px;
+  color: white;
+}
+.navbar-brand{
+  font-size: 30px;
+}
+</style>
---
+++ vue.js
@@ -0,0 +1,25 @@
+//  <script>
+//         // Initialize Firebase
+//         var config = {
+//             apiKey: "AIzaSyAEOzqJV9-9TmY--fp6XYcNdvsDnYWJEQk",
+//             authDomain: "kanban-a65f5.firebaseapp.com",
+//             databaseURL: "https://kanban-a65f5.firebaseio.com",
+//             projectId: "kanban-a65f5",
+//             storageBucket: "kanban-a65f5.appspot.com",
+//             messagingSenderId: "165180069193"
+//         };
+//         firebase.initializeApp(config);
+//     </script>
+
+const app = new Vue({
+    el: "#app",
+    body: {
+        
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+})
